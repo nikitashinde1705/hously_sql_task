51bb904c71a3aea752d7526b8fdac5cdb0cd98de
Nikita Shinde <nikitashinde1705@gmail.com>
2026-03-30 20:52:32 +0530

    added frontend deployed link in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const sequelize = require("./config/db");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  //origin: "http://localhost:5173",
+  origin:"https://houslysql.netlify.app",
   credentials: true
 }));
 app.use(express.json());
